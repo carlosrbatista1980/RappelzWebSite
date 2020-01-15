eaa38993140d28b745fb3097ae9bbfcb2e0e6d03
Carlos Rodrigues Batista <carlosrbatista1980@gmail.com>
2020-01-15 18:50:36 -0300

    creating structures

--- RappelzWebSite/Controllers/HomeController.cs
+++ RappelzWebSite/Controllers/HomeController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using RappelzWebSite.Models;
+using AutoMapper.Mappers;
 
 namespace RappelzWebSite.Controllers
 {
@@ -13,6 +17,33 @@ namespace RappelzWebSite.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        
+        [HttpPost]
+        public IActionResult GetResult(FormTestViewModel formTestViewModel)
+        {
+            /*
+
+            var ftm = new FormTestModel();
+            var mapConfig = new MapperConfiguration(cfg => cfg.CreateMap<FormTestViewModel, FormTestModel>());
+            var _formTestViewModel = formTestViewModel;
+            var maper = mapConfig.CreateMapper();
+
+            maper.Map(_formTestViewModel, ftm);
+
+            ValuesController vcController = new ValuesController();
+            vcController.InsertResult(ftm);
+
+            mapConfig = new MapperConfiguration(cfg => cfg.CreateMap<FormTestModel, FormTestViewModel>());
+            maper = mapConfig.CreateMapper();
+
+            maper.Map(ftm, _formTestViewModel);
+
+            formTestViewModel = _formTestViewModel;
+            */
+
+            return View(nameof(Index));
+
         }
 
         public IActionResult Privacy()
---
+++ RappelzWebSite/Models/FormTestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RappelzWebSite.Models
+{
+    public class FormTestViewModel
+    {
+        public string TextNote { get; set; }
+        public string TextEmail { get; set; }
+        public string TextEma { get; set; }
+    }
+}
